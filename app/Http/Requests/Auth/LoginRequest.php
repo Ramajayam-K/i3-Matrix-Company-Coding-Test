20211aa575a5fee8e5c67cd0acb743349afca6c3
Ramajayam-K <ramajayamkasi11@gmail.com>
2025-05-05 11:00:22 +0530

    Validation not displaying is fixed

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -45,7 +45,7 @@ class LoginRequest extends FormRequest
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'a' => 'Invalid Username or password.',
+                'InvalidAcccount' => 'Invalid Username or password.',
             ]);
         }
 
@@ -80,6 +80,6 @@ class LoginRequest extends FormRequest
      */
     public function throttleKey(): string
     {
-        return Str::transliterate(Str::lower($this->string('username')).'|'.$this->ip());
+        return Str::transliterate(Str::lower($this->string('username')) . '|' . $this->ip());
     }
 }
